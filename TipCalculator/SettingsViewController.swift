091b7b60ce337b236568522a156db3bceba49431
Jimmy Chen <jimmy@Jimmys-MBP.lan>
2017-03-10 17:14:40 -0700

    adding code path code to git

---
+++ TipCalculator/SettingsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SettingsViewController.swift
+//  TipCalculator
+//
+//  Created by Jimmy Chen on 3/10/17.
+//  Copyright © 2017 Jimmy Chen. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var defaultTipField: UITextField!
+    @IBOutlet weak var colorThemeSwitch: UISwitch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+
+    @IBAction func colorThemeSwitchChange(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(colorThemeSwitch.isOn, forKey:"color_theme")
+        defaults.synchronize()
+    }
+    @IBAction func defaultTipPercentageChange(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        let defaultTip = Float(defaultTipField.text!) ?? 20.0
+        defaults.set(defaultTip, forKey:"default_tip")
+        print("default_tip: " + defaults.string(forKey: "default_tip")!)
+        defaults.synchronize()
+    }
+    
+
+}
---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Jimmy Chen on 3/3/17.
+//  Copyright © 2017 Jimmy Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+   
+
+
+    @IBOutlet weak var tipValueLabel: UILabel!
+    @IBOutlet weak var tipSlider: UISlider!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let bill = Double(billField.text!) ?? 0
+        let tipPercentage:Double = Double(Int(tipSlider.value));
+        tipValueLabel.text = String(format: "%.0f", tipPercentage)
+        let tip = bill * (tipPercentage/100);
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        let index:Double = Double(segmentedControl.selectedSegmentIndex)
+        splitLabel.text = String(format: "$%.2f", total/(index+1))
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = Foundation.UserDefaults.standard
+        let tipValue = defaults.integer(forKey:"default_tip")
+        tipSlider.value = Float(tipValue)
+        tipValueLabel.text = String(format: "%.0f", tipValue)
+
+    }
+    override func viewDidAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+
+    }
+    
+
+}
+
